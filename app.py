c3b27b855275f02231a3adf8572bbaeac7abbaf1
MattyAtown <mattauton22@gmail.com>
2025-05-13 00:51:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
+
+@app.route("/service-catalogue.html")
+def service_catalogue():
+    return render_template("service-catalogue.html")
+
+@app.route("/company-values.html")
+def company_values():
+    return render_template("company-values.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
