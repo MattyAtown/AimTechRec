8a4cec94e76c8e307d061fada8279b5e63f9c840
MattyAtown <mattauton22@gmail.com>
2025-05-13 19:03:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/services")
+@app.route('/services')
 def services():
-    return render_template("services.html")
+    return render_template('services.html')
 
-@app.route("/values")
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/values')
 def values():
-    return render_template("values.html")
+    return render_template('values.html')
 
-@app.route("/static/<path:path>")
-def static_files(path):
-    return app.send_static_file(path)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
