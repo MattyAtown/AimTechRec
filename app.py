57308abcc4fae9b8f322f91cae7aaf5473a0416b
MattyAtown <mattauton22@gmail.com>
2025-05-13 19:16:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -31,4 +32,5 @@ def values():
     return render_template('values.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the port Render provides
+    app.run(host="0.0.0.0", port=port, debug=True)
