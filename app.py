19216879ac137f2f4ae9f7d806c1ec54386813ba
MattyAtown <mattauton22@gmail.com>
2025-05-14 13:04:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,6 @@ def logout():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+    with app.app_context():
     db.create_all()
     app.run(host="0.0.0.0", port=port, debug=True)
