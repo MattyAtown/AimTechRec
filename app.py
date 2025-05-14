5ed82856aed5f07bc91b2042078d89275c3df2ef
MattyAtown <mattauton22@gmail.com>
2025-05-14 13:28:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def home():
