2c35fdb398aa3bd634c9dc9099703ae894902727
MattyAtown <mattauton22@gmail.com>
2025-05-29 01:01:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-]from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
