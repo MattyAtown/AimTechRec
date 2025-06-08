377ca931ccf80674e5f2c80bc213018f6fd74228
MattyAtown <mattauton22@gmail.com>
2025-06-09 00:48:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_cors import CORS
 import requests
 import os
