ce9864f35c53687347b539bd64fbc23f19f08ed1
MattyAtown <mattauton22@gmail.com>
2025-06-08 22:28:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-app.secret_key = "your_secret_key"
 from flask_cors import CORS
 import requests
 import os
 import fitz  # PyMuPDF
 import openai
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__)                    
+app.secret_key = "e8c3b90a9d5c407c9e12311cfec4cdbc"  
 
 # Environment variables
 ADZUNA_APP_ID = os.getenv("ADZUNA_APP_ID")
