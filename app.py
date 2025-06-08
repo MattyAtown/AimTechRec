c462bd80a44cdb10956efd3f478a162da400502a
MattyAtown <mattauton22@gmail.com>
2025-06-09 00:53:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class User(db.Model):
     name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    cv_text = db.Column(db.Text)  
 
 # Environment variables
 ADZUNA_APP_ID = os.getenv("ADZUNA_APP_ID")
