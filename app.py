c561c81c7373147aae01be546a06c67064d03ca3
MattyAtown <mattauton22@gmail.com>
2025-06-09 18:38:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///aimtechrec.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = "uploads" 
 
 db = SQLAlchemy(app)
 
